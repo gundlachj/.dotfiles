b9e9352de804175eafcc9a0860ed17c2dece1ec4
gundlachj <gundlachj@msoe.edu>
2025-08-15 01:12:59 -0400

    added lua_ls, cmake, and bashls capabilities setups

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -32,6 +32,15 @@ return {
       lspconfig.clangd.setup({
         capabilities = capabilities
       })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.cmake.setup({
+        capabilities = capabilities
+      })
+      lspconfig.bashls.setup({
+        capabilities = capabilities
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
