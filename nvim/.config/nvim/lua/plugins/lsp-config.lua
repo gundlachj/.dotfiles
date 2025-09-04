075bf08ac49a907fe80e012264ebd9b54d519a00
gundlachj <gundlachj@msoe.edu>
2025-09-03 23:29:26 -0500

    added jdtls

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -41,6 +41,9 @@ return {
       lspconfig.bashls.setup({
         capabilities = capabilities
       })
+      lspconfig.jdtls.setup({
+        capabilities = capabilities
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
